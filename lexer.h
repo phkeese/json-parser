dd4fa527b0da9a4d8501acd5e10e37ce6d3856f7
Philipp Keese <philipp.keese@student.hpi.de>
2020-10-23 14:25:02 +0200

    add lexer interface

---
+++ lexer.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <istream>
+
+namespace json {
+enum token_type {
+	// Meta
+	T_ERROR,
+	T_EOF,
+
+	// Non-data
+	T_LBRACE,
+	T_RBRACE,
+	T_LBRACKET,
+	T_RBRACKET,
+	T_COMMA,
+	T_COLON,
+
+	// Data
+	T_STRING,
+	T_NUMBER,
+	T_TRUE,
+	T_FALSE,
+	T_NULL,
+};
+
+struct Token {
+	token_type type;
+	int start, length;
+};
+
+class Lexer {
+  public:
+	Lexer(std::istream &stream);
+	virtual ~Lexer();
+
+	Token get_next();
+
+  private:
+	Token make_token(token_type);
+	int advance();
+	int peek();
+	int prev();
+	void skip_whitespace();
+	std::istream &stream;
+	int offset, length;
+};
+} // namespace json
