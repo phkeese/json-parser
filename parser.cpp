f451ca06c663a17d2378107421efb1c41179b3ce
Philipp Keese <philipp.keese@student.hpi.de>
2020-10-23 23:18:31 +0200

    implement parse_object, parse_array

--- parser.cpp
+++ parser.cpp
@@ -24,6 +24,51 @@ Value::Ptr Parser::parse() {
 	}
 }
 
+// Finish parsing an object
+ValueObject::Ptr Parser::parse_object() {
+	ValueObject::data_type data{};
+	while (peek.type != T_RBRACE) {
+		// Require string key
+		if (!consume(T_STRING)) {
+			return nullptr;
+		}
+		std::string key_literal = get_literal(prev);
+		std::string key = key_literal.substr(1, key_literal.length() - 2);
+		// Require key : value
+		if (!consume(T_COLON)) {
+			return nullptr;
+		}
+		Value::Ptr value = parse();
+		data[key] = value;
+
+		// Require comma after key : value
+		if (!consume(T_COMMA)) {
+			return nullptr;
+		}
+	}
+	// Closing brace
+	consume(T_RBRACE);
+	return std::make_shared<ValueObject>(data);
+}
+
+// Finish parsing an array
+ValueArray::Ptr Parser::parse_array() {
+	ValueArray::data_type data{};
+	while (peek.type != T_RBRACKET) {
+		Value::Ptr value = parse();
+		data.push_back(value);
+		// Require comma between values
+		if (!consume(T_COMMA)) {
+			return nullptr;
+		}
+	}
+	// All closing bracket
+	if (!consume(T_RBRACKET)) {
+		return nullptr;
+	}
+	return std::make_shared<ValueArray>(data);
+}
+
 Value::Ptr Parser::parse_literal() {
 	switch (prev.type) {
 	case T_NULL:
