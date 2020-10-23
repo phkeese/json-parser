f74d3d0fa6416d09aaca621ef2bd0a985ea2efd1
Philipp Keese <philipp.keese@student.hpi.de>
2020-10-24 00:05:09 +0200

    comprehensive testing of simple object

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -24,5 +24,22 @@ int main() {
 		Value::Ptr value = parser.parse();
 		assert(value);
 		assert(value->type == V_OBJECT);
+
+		ValueObject &object = dynamic_cast<ValueObject &>(*value);
+		assert(object.data.find("key") != object.data.end());
+
+		Value::Ptr null_value = object.data["key"];
+		assert(null_value);
+		assert(null_value->type == V_NULL);
+	}
+
+	// Complex object
+	{
+		std::stringstream stream;
+		stream << "{\"key\" : [1,true,null,false]}";
+		Parser parser{stream};
+		Value::Ptr value = parser.parse();
+		assert(value);
+		assert(value->type == V_OBJECT);
 	}
 }
