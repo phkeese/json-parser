7eefeaceefc47801a29f10692a9a903d61a35786
Philipp Keese <philipp.keese@student.hpi.de>
2020-10-23 14:37:06 +0200

    implement basic lexing (single-char)

--- lexer.cpp
+++ lexer.cpp
@@ -6,6 +6,45 @@ Lexer::Lexer(std::istream &stream) : stream{stream}, offset{0}, length{0} {}
 
 Lexer::~Lexer() {}
 
+Token Lexer::get_next() {
+	skip_whitespace();
+	int c = advance();
+	switch (c)
+	{
+	case EOF:
+		return make_token(T_EOF);
+		break;
+	case '{':
+		return make_token(T_LBRACE);
+		break;
+	case '}':
+		return make_token(T_RBRACE);
+		break;
+	case '[':
+		return make_token(T_LBRACKET);
+		break;
+	case ']':
+		return make_token(T_RBRACKET);
+		break;
+	case ',':
+		return make_token(T_COMMA);
+		break;
+	case ':':
+		return make_token(T_COLON);
+		break;
+	default:
+		return make_token(T_ERROR);
+		break;
+	}
+}
+
+Token Lexer::make_token(token_type type) {
+	Token token{type, offset, length};
+	offset += length;
+	length = 0;
+	return token;
+}
+
 int Lexer::advance() {
 	if (!stream.good()) {
 		return EOF;
@@ -28,6 +67,8 @@ void Lexer::skip_whitespace() {
 	while (isspace(peek())) {
 		advance();
 	}
+	offset += length;
+	length = 0;
 }
 
 } // namespace json
