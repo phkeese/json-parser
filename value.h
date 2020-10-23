eeab8de3694820b1346ae0983280b80b83dc5cc1
Philipp Keese <philipp.keese@student.hpi.de>
2020-10-23 16:56:10 +0200

    define value types

---
+++ value.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace json {
+
+enum value_type {
+	V_NULL,
+	V_OBJECT,
+	V_ARRAY,
+	V_NUMBER,
+	V_STRING,
+	V_BOOL,
+};
+
+struct Value {
+	Value(value_type type);
+	virtual ~Value();
+	const value_type type;
+};
+
+using ValuePtr = std::shared_ptr<Value>;
+
+namespace {
+template <typename T, value_type type> struct ValueType : public Value {
+	ValueType(T data) : Value{type}, data{data} {};
+	T data;
+};
+} // namespace
+
+using ValueNumber = ValueType<double, V_NUMBER>;
+using ValueString = ValueType<std::string, V_STRING>;
+using ValueBool = ValueType<bool, V_BOOL>;
+using ValueArray = ValueType<std::vector<ValuePtr>, V_ARRAY>;
+using ValueObject = ValueType<std::map<ValueString, ValuePtr>, V_OBJECT>;
+
+} // namespace json
